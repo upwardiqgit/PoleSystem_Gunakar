7f02134802ad22b7c9c5f569b72aa0d39ae9d942
gunakarreddy1555 <gunakarreddy15559@gmail.com>
2024-12-25 13:06:35 +0530

    addes src

---
+++ src/main/java/com/upwardiq/guna/UpwardIqPollingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.upwardiq.guna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpwardIqPollingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpwardIqPollingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upwardiq/guna/UpwardIqPollingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upwardiq.guna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpwardIqPollingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
