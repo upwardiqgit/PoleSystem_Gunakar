0c476c37523a1b01f976e1cf1ad9a95c92c35e78
gunakarreddy1555 <gunakarreddy15559@gmail.com>
2024-12-25 20:06:11 +0530

    created front end pages and provided connection

--- src/main/java/com/upwardiq/guna/Controller/UserController.java
+++ src/main/java/com/upwardiq/guna/Controller/UserController.java
@@ -1,9 +1,17 @@
 package com.upwardiq.guna.Controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.upwardiq.guna.Entity.User;
 import com.upwardiq.guna.Service.UserService;
 import com.upwardiq.guna.Service.VoteService;
 
@@ -26,5 +34,19 @@ public class UserController {
         return "registerpage";
     }
     
+    @PostMapping("/register")
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> registerUser(@ModelAttribute User user) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            // Save the user (implement your saving logic here)
+            service.saveUser(user);
+            response.put("status", "success");
+        } catch (Exception e) {
+            response.put("status", "failure");
+        }
+        return ResponseEntity.ok(response);  // Return the response as JSON
+    }
+
 }
 
